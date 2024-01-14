432efbf52ead44adcd59a01cb6de7599c4be7174
fan4319693 <2532879773@qq.com>
2024-01-14 19:25:28 +0800

    commit

---
+++ IdeaProjects/test/src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @description:
+ * @author: 范帅
+ * @create: 2024-01-14
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
